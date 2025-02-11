d6f5dd7aa7bd1be9a31faad724337c51cceb09f4
Naveen S <naveen121101@gmail.com>
2025-02-11 12:50:48 +0530

    Example

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: import.meta.env.VITE_GOOGLE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
   databaseURL: import.meta.env.VITE_DATABASE_URL,
-  projectId: import.meta.env.VITE_PROJECT_ID,
+  projectId: "counselling-system-7d104",
   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_APP_ID
